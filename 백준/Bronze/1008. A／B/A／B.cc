6d29ebbb72da81532ce4a8d123fea1df9c1fc6d8
Navifra-Hank <hank@navifra.com>
2024-11-25 17:09:02 +0900

    Update A／B.cc

--- 백준/Bronze/1008. A／B/A／B.cc
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <cstdio> // printf를 사용하기 위해 필요
+#include <cstdio> 
 
 int main() {
     double A, B;
     std::cin >> A >> B;
 
-    // A / B 결과를 소수점 15자리까지 출력
+   
     printf("%.15f\n", A / B);
 
     return 0;
